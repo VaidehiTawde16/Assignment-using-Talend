c9a8d053d5d8590c2cc681de8887d9562d008162
Vaidehi Tawde <vaideheetawde@gmail.com>
2024-06-10 16:19:39 -0400

    Add files via upload

---
+++ Assignment_using_Talend.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE Product_wise_account_transfers;
+
+USE Product_wise_account_transfers;
+
+select * from product;
+SELECT COUNT(*) FROM product;
+select * from account;
+SELECT COUNT(*) FROM account;
+select * from transfer;
+SELECT COUNT(*) FROM transfer;
+select * from final;
+SELECT COUNT(*) FROM final;
